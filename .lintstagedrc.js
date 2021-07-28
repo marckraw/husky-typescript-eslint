10bb1e7faaa4b98d6625bc43180d830ad9724b3e
Marcin Krawczyk <marckraw@icloud.com>
2021-07-28 13:57:42 +0200

    Custom tsconfig

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
     './src/**/*.{js,jsx}': ['prettier --write', 'yarn eslint:fix:js'],
-    './src/**/*.{ts,tsx}': ['prettier --write', 'yarn eslint:fix:ts', "yarn build"],
+    './src/**/*.{ts,tsx}': ['prettier --write', 'yarn eslint:fix:ts', 'yarn build'],
 }
