b07b4dc8f01f623e6028f935478b224059eb8299
Marcin Krawczyk <marckraw@icloud.com>
2021-07-28 12:33:13 +0200

    Set up husky@7, lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "*.{js,jsx}": "yarn eslint:fix:js",
+    "*.{ts,tsx}": "yarn eslint:fix:js"
+};
---
+++ index.ts
@@ -0,0 +1,6 @@
+const dupa = 'asdasd';
+
+const something = 'jbdkjfhsf';
+
+console.log(dupa);
+console.log(something);
