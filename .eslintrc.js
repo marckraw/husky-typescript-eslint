efcbf22b0e56ab772712eaeecf333f951d0f5291
Marcin Krawczyk <marckraw@icloud.com>
2021-07-28 13:52:01 +0200

    TSC build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
     "root": true,
-    // env: {
-    //     browser: true,
-    //     node: true,
-    // },
     "parser": "@typescript-eslint/parser",
     "plugins": [
         "@typescript-eslint"
