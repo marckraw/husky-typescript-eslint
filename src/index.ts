04c508052bd77b54f2903396c5bc67f3f1d90b08
Marcin Krawczyk <marckraw@icloud.com>
2021-10-17 12:00:49 +0200

    chore(deps): update

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,10 @@
 // something('hello');
 
 const something = 'asdasd'
+const somethingDifferent: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+]
+
+console.log(somethingDifferent)
 
 console.log(something)
